bb8c8183ee7b1be1cdd99508a2dbb0bc7c15c8a2
maloiMASLACH <60549661+maloiMASLACH@users.noreply.github.com>
2020-03-17 13:37:54 +0200

    feat: solve solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
+  if ((!array)||(array.length==0))
   return 0;
+else
+ return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
+  if ((!array)||(array.length==0))
   return 0;
+  else
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
+  if ((!array)||(array.length==0))
   return 0;
+else
+{
+  var s=0;
+for (var i=0;i<array.length;i++)
+s=s+array[i];
+
+return s/array.length;
+}
 }
